ccb686529ebb9fa1cc4f9ba03e90373e1350d57d
Thomas LeMarrec <thomas.lemarrec@wolterskluwer.com>
2024-05-22 12:15:55 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/api/send-description', async function (req, res) {
+    var description = "The description is a sample description";
+    try {
+        const response = await axios.post('https://model-serving-dispatcher-mirj.azurewebsites.net/api/smartformapi/suggest/spf/imsevent', {
+            description: description,
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data from DataLab API:', error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
